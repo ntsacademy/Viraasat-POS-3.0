662b672931e2258d7d1165c95ff9a5f255cb8ea4
ntsacademy <158406243+ntsacademy@users.noreply.github.com>
2026-08-13 10:31:46 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "viraasat-pos-3.0-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
